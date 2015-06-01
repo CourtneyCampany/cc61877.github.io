cdff60d2b3e96e916608cb7bfae20580ee3f09dd
Courtney Campany <courtneycampany@gmail.com>
2015-06-01 10:06:15 +1000

    first commit

---
+++ talk_setup.R
@@ -0,0 +1,17 @@
+
+library(devtools)
+
+install.packages('stringr')
+devtools::install_github('muschellij2/slidify')
+
+
+#install_github('ramnathv/slidify', ref = github_pull(425))
+
+install_github("slidifyLibraries", "ramnathv")
+library(slidify)
+
+author('wtc3_talk')
+
+
+slidify("index.Rmd")
+
